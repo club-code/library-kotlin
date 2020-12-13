3c3e5ebac6f2e9c8ab876b761c1acfe7906bd5d3
Al3xCalibur <alexandre.conte11@gmail.com>
2020-12-13 13:47:26 +0100

    Add euclid, lcm, gcd

--- src/main/kotlin/org/clubcode/library/number/Int.kt
+++ src/main/kotlin/org/clubcode/library/number/Int.kt
@@ -1,10 +1,31 @@
 package org.clubcode.library.number
 
-fun Int.pow(pow: Int): Int {
-    return when {
+fun Int.pow(pow: Int): Int =
+    when {
         pow == 0 -> 1
         pow == 1 -> this
         pow and 1 == 0 -> (this * this).pow(pow / 2)
         else -> this * ((this * this).pow(pow / 2))
     }
-}
+
+tailrec fun gcd(a: Int, b: Int): Int =
+    if (b == 0)
+        a
+    else
+        gcd(b, a%b)
+
+
+fun lcm(a: Int, b: Int): Int = kotlin.math.abs(a * b)/gcd(a, b)
+
+/**
+ * @return (d, u, v) where d is the gcd of a and b, u and v two integers with au + bv = d
+ */
+fun euclid(a: Int, b: Int): Euclid =
+    if (b == 0)
+        Euclid(a, 1, 0)
+    else {
+        val r = euclid(b, a % b)
+        Euclid(r.gcd, r.v, r.u - (a/b)*r.v)
+    }
+
+data class Euclid(val gcd: Int, val u: Int, val v: Int)
