4c2c480623c124ab5794ae235fbcd38c919e4ffc
Al3xCalibur <alexandre.conte11@gmail.com>
2020-12-29 14:01:44 +0100

    Add edge equals

--- src/main/kotlin/org/clubcode/library/graph/Edge.kt
+++ src/main/kotlin/org/clubcode/library/graph/Edge.kt
@@ -8,6 +8,24 @@ open class Edge<T>(val from: T, val to: T) {
     override fun toString(): String {
         return "($from -> $to)"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Edge<*>) return false
+
+        if (from != other.from) return false
+        if (to != other.to) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = from?.hashCode() ?: 0
+        result = 31 * result + (to?.hashCode() ?: 0)
+        return result
+    }
+
+
 }
 
 class ValueEdge<T, K>(from: T, to: T, var weight: K): Edge<T>(from, to) {
